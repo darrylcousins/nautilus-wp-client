ef77d3b11066d50e9001c15a975b21fe3e809af5
darrylcousins <darryljcousins@gmail.com>
2019-06-08 22:19:19 +1200

    First commit

---
+++ nautilusbraids.md
@@ -0,0 +1,53 @@
+# Nautilus Braids website update to Worpress
+
+## Task
+
+Upgrade [Nautilus Braids] website to [Wordpress].
+
+## 01 June 2019 (6 hrs)
+
+### Set up development server
+
+* install [Nginx] web server
+* install [PHP]
+* install [MariaDB]
+* install [Wordpress]
+* install [Graphql] plugin: [wp-graphql]
+* install [Graphiql] browser: [wp-graphiql]
+* set up [permalinks] using [nginx location]
+* install and configure [jwt authentication] for data import
+
+## 02 June 2019 (4hrs)
+
+* start working on script to [scrape] the current webiste for text and import
+  into [Wordpress] database.
+
+## 07 June 2019 (10hrs)
+
+* sort out graphql connections, authorization, and queries
+* successfully imported current text into [Wordpress] database
+* build barebones theme for [Wordpress]
+* begin building front-end application with [React]
+
+## 08 June 2019 (3hrs)
+
+* initial routing built using [React Router]
+* theming with [React Semantic UI] and [Tachyons]
+
+[Nautilus Braids]: https://nautilusbraids.co.nz
+[Wordpress]: https://wordpress.org
+[Nginx]: https://nginx.org
+[PHP]: https://php.net
+[MariaDB]: https://mariadb.org
+[MySQL]: https://mysql.com
+[Graphql]: https://graphql.org
+[wp-graphql]: https://github.com/wp-graphql/wp-graphql
+[wp-graphiql]: https://github.com/wp-graphql/wp-graphiql
+[permalinks]: https://wordpress.org/support/aricale/using-permalinks
+[nginx location]: https://nginxlibrary.com/wordpress-permalinks
+[scrape]: https://en.wikipedia.org/wiki/Web_scraping
+[jwt authenticaition]: https://github.com/wp-graphql/wp-graphql-jwt-authentication
+[React]: https://reactjs.org
+[React Router]: https://reacttraining.com/react-router/
+[React Semantic UI]: https://react.semantic-ui.com
+[Tachyons]: https://tachyons.io/
--- src/App.js
+++ src/App.js
@@ -1,26 +1,191 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Suspense, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
+const graphqlEndpoint = 'http://10.42.0.98/graphql';
+
+/*
+ * helper function for http fetch requests
+ */
+const fetchPromise = (body, authToken) => {
+  let headers = {}
+  headers['Content-Type'] = 'application/json';
+  if (authToken) headers['Authorization'] = `Bearer ${authToken}`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    fetch(graphqlEndpoint, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: headers,
+      body: body
+    })
+  )
 }
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <HeaderMenu />
+        <Container>
+          <Route path="/:slug" component={ Page } />
+          <Route strict exact path="/" component={ Page } />
+        </Container>
+        <FooterMenu />
+      </Router>
+    )
+  }
+
+}
+
+class PageWrapper extends Component {
+
+}
+
+/* therefore must need a page wrapper to pass slug to the page */
+class Page extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      externalData: {
+        title: "",
+        date: "",
+        slug: "",
+        excerpt: "",
+        content: ""
+      }
+    }
+  }
+
+  componentDidMount() {
+    let slug = this._getSlugFromMatch(this.props);
+    console.log('did mount: ', slug);
+    this._loadAsyncData(slug);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params !== this.props.match.params) {
+      this.setState({externalData: null});
+      let slug = this._getSlugFromMatch(nextProps);
+      console.log('will receive props: ', slug);
+      this._loadAsyncData(slug);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this._asyncRequest && 'cancel' in this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+
+  render() {
+    // if (this.state.loading) return <div>loading ...</div>
+    const data = this.state.externalData;
+    if (this.state.externalData === null) return <div>Loading ...</div>
+    return (
+        <div>
+          <header>
+            { data.title } - { data.date } - { data.slug }
+            <aside
+              dangerouslySetInnerHTML={{ __html: data.excerpt }}
+            />
+          </header>
+          <content
+              dangerouslySetInnerHTML={{ __html: data.content }}
+            />
+        </div>
+    );
+  }
+
+  _getSlugFromMatch(props) {
+    console.log(props.match)
+    const urlParams = props.match.params;
+    const slug = 'slug' in urlParams ? urlParams.slug : 'index';
+    return slug
+  }
+
+  _loadAsyncData(slug) {
+    console.log('load data', slug);
+    let body = JSON.stringify({
+      variables: {
+        "first": 1,
+        "where": {name: slug}
+      },
+      query: `
+        query GET_PAGES($first: Int, $where: RootQueryToPageConnectionWhereArgs!) {
+          pages(first: $first, where: $where) {
+            edges {
+              node {
+                title
+                slug
+                content
+                excerpt
+                date
+              }
+            }
+          }
+        }
+      `,
+    })
+
+    this._asyncRequest = fetchPromise(body)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ externalData: res.data.pages.edges[0].node });
+        console.log(res.data.pages.edges[0].node);
+        //this.setState({ title: title, date, content, excerpt, loading: false });
+      })
+      .catch((error) => console.error(error));
+  }
+}
+
+class Menu extends Component {
+
+  render() {
+    return (
+      <menu id="footer">
+        <ul>
+          { this.pages.map(page => (
+            <li key={ page.slug }>
+              <Link to={ page.slug }>
+                { page.title }
+              </Link>
+            </li>
+          ))}
+          </ul>
+      </menu>
+    );
+  }
+}
+
+class HeaderMenu extends Menu {
+
+  constructor(props) {
+    super(props);
+    this.pages = [
+      { title: 'Home', slug: '/index' },
+      { title: 'Braided Ropes', slug: '/braided-rope' },
+      { title: 'Products', slug: '/products' },
+      { title: 'ProductInformation', slug: '/product-information' },
+      { title: 'Technical Data', slug: '/technical-data' },
+      { title: 'Distributors', slug: '/distributors' },
+      { title: 'About Nautilus Braids', slug: '/about' }
+    ]
+  }
+
+}
+class FooterMenu extends Menu {
+
+  constructor(props) {
+    super(props);
+    this.pages = [
+      { title: 'Terms of Use', slug: '/terms-of-use' },
+      { title: 'Privacy Policy', slug: '/privacy-policy' }
+    ]
+  }
+
+}
+
 
 export default App;
